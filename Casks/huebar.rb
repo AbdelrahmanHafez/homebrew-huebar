4c1ece2885c02e244fdeb66c8f1498a714a5dbeb
Hafez <a.hafez852@gmail.com>
2026-05-12 02:33:19 +0200

    fix(cask): drop unnecessary verified: and 'macOS' from desc per brew audit

--- Casks/huebar.rb
+++ Casks/huebar.rb
@@ -2,10 +2,9 @@ cask "huebar" do
   version "0.1.17"
   sha256 "b2722b0ca7f4ec9405c8caee333f4ace3b0a10e55820160b7efebcee4e5a5b5e"
 
-  url "https://github.com/jurre/huebar/releases/download/v#{version}/HueBar.zip",
-      verified: "github.com/jurre/huebar/"
+  url "https://github.com/jurre/huebar/releases/download/v#{version}/HueBar.zip"
   name "HueBar"
-  desc "Native macOS menubar app for controlling Philips Hue lights"
+  desc "Menu bar app for controlling Philips Hue lights"
   homepage "https://github.com/jurre/huebar"
 
   livecheck do
